18954329357ed853028f049fdfcde07b349d1608
Nurdaulet <alibek4383@gmail.com>
2025-04-05 20:35:17 +0600

    Created HighResImage class

---
+++ HighResImage.java
@@ -0,0 +1,15 @@
+public class HighResImage implements Image{
+    private String name;
+    public HighResImage(String name){
+        this.name = name;
+    }
+    private void loadImage(){
+        System.out.println("Loading full-resolution images: " + name);
+    }
+    public void displayThumb(){
+        System.out.println("Displaying thumbnail for: " + name);
+    }
+    public void displayFullImage(){
+        System.out.println("Displaying full image for: " + name);
+    }
+}
